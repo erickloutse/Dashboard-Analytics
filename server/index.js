e9111ac5b9d3bb617ff7dc000bc14b14117076fe
erickloutse <erickloutse@gmail.com>
2025-03-27 10:05:35 +0000

    Update

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import connectDB from "./config/mongodb.js";
 
-// All Routes
+// Routes
 import visitsRoutes from "./routes/visits.js";
 import pagesRoutes from "./routes/pages.js";
 import statsRoutes from "./routes/stats.js";
